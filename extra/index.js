81f085c6531fd7a374aa94686e9db7759cafa6b0
Bharat Bhardwaj <bharat.bhardwaj@bharatr105.successivetech.com>
2021-05-20 13:51:53 +0530

    added the pattern

--- extra/index.js
+++ extra/index.js
@@ -1,6 +1,8 @@
 import {hasPermissions,validateUsers} from '../utils/index';
 import {users,permissions} from './constants';
+import {diamond2,diamondPattern} from '../patterns/pattern';
 
 console.log(hasPermissions('getUsers','trainee','read',permissions));
 validateUsers(users);
+console.log(diamond2,diamondPattern)
 
