c7df627514b44a808b7698b1ff3a225266bd7a14
Bharat Bhardwaj <bharat.bhardwaj@bharatr105.successivetech.com>
2021-05-25 10:08:27 +0530

    added the authentication and authorization

--- dist/src/server.js
+++ dist/src/server.js
@@ -4,6 +4,8 @@ exports.Server = void 0;
 const bodyParser = require("body-parser");
 const errorHandler_1 = require("./libs/errorHandler");
 const notFoundRoute_1 = require("./libs/notFoundRoute");
+const configMiddelware_1 = require("./libs/configMiddelware");
+const authMiddleWare_1 = require("./libs/routes/authMiddleWare");
 const router_1 = require("./router");
 class Server {
     constructor(config, app) {
@@ -13,8 +15,10 @@ class Server {
     initBodyParser() {
         this.app.use(bodyParser.urlencoded({ extended: false }));
         // parse application/json
-        this.setupRoutes();
         this.app.use(bodyParser.json());
+        this.app.use(configMiddelware_1.configMiddelware);
+        this.app.use(authMiddleWare_1.authMiddleware);
+        this.setupRoutes();
         this.app.use(notFoundRoute_1.notFoundRoutesMiddleware);
         this.app.use(errorHandler_1.errorHandlerMiddleware);
     }
@@ -31,6 +35,7 @@ class Server {
     }
     setupRoutes() {
         this.app.get('/health-check', (req, res) => {
+            console.log(req.config);
             res.send('i am OK');
         });
         this.app.use('/api', router_1.router);
